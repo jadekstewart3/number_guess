07c070f2b4d09ef757c267d4c1c923dd8127248c
Jade Stewart <jadekstewart3@gmail.com>
2024-04-22 11:20:49 -0600

    Add more game logic- need to fix updating of score

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 'use strict';
+const number = Math.trunc(Math.random() *20) + 1;
+let score = 20;
 
+document.querySelector('.number').textContent = number 
 document.querySelector('.check').addEventListener('click', function(){
   const guess = Number(document.querySelector('.guess').value);
 
   if(!guess){
     document.querySelector('.message').textContent = 'No number!';
-  }
+  } else if(guess === number) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if(guess > number) {
+    if(score > 1){
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message') = 'You lose!'
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < number) {
+    if(score > 1){
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message') = 'You lose!'
+      document.querySelector('.score').textContent = 0;
+    }
+  } 
 });
