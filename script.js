c34f515db63bd8c0888ece58746e282ab296399d
Jade Stewart <jadekstewart3@gmail.com>
2024-04-23 12:08:10 -0600

    Add dynamic css styling to game when a player wins

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ document.querySelector('.number').textContent = number;
 document.querySelector('.check').addEventListener('click', function(){
   const guess = Number(document.querySelector('.guess').value);
 
+  // no input
   if(!guess){
     document.querySelector('.message').textContent = 'No number!';
+    // player wins
   } else if(guess === number) {
     document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem'
+    // guess is too high
   } else if(guess > number) {
     if(score > 1){
       document.querySelector('.message').textContent = 'Too High!';
@@ -20,6 +27,7 @@ document.querySelector('.check').addEventListener('click', function(){
       document.querySelector('.message').textContent = 'You lose!'
       document.querySelector('.score').textContent = 0;
     }
+    // guess is too low
   } else if (guess < number) {
     if(score > 1){
       document.querySelector('.message').textContent = 'Too Low!';
